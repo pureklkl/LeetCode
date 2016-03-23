a85655b4431fc3f2da39b91e7ae8340b2cec83fd
pureklkl <pureklkl2@hotmail.com>
2016-03-22 18:59:31 -0700

    Create LongestCommonPrefix

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,24 @@
+public class Solution {
+    /**
+    * straight forward...
+    *
+    */
+    public String longestCommonPrefix(String[] strs) {
+        int i=0;
+        String res="";
+        if(strs.length==0)
+            return res;
+        while(true){
+            if(i>=strs[0].length())
+                return res;
+            char b = strs[0].charAt(i);
+            for(int sn=1;sn<strs.length;sn++){
+                if(i>=strs[sn].length()||b!=strs[sn].charAt(i)){
+                    return res;
+                }
+            }
+            res=res+b;
+            i++;
+        }
+    }
+}
